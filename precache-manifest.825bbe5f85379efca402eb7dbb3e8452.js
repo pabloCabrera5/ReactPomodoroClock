c8c95948aed39aa6c560eb18ec67d979e3b1e3b5
Pablo CABRERA PEREZ <pablo.cabrera@alten.es>
2020-05-10 23:20:53 +0200

    Updates

---
+++ precache-manifest.825bbe5f85379efca402eb7dbb3e8452.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49cd4dd10266fe19965ecd3f63c78423",
+    "url": "/ReactPomodoroClock/index.html"
+  },
+  {
+    "revision": "b42906d1066298028d70",
+    "url": "/ReactPomodoroClock/static/css/main.46043796.chunk.css"
+  },
+  {
+    "revision": "59c27aaf3e9037e4422e",
+    "url": "/ReactPomodoroClock/static/js/2.cdc0cb87.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactPomodoroClock/static/js/2.cdc0cb87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b42906d1066298028d70",
+    "url": "/ReactPomodoroClock/static/js/main.cd7cb52c.chunk.js"
+  },
+  {
+    "revision": "933bae44c2c55b353fcf",
+    "url": "/ReactPomodoroClock/static/js/runtime-main.95a944af.js"
+  }
+]);
